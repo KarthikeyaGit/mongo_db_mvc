a9d1553ee5e2133400cea2ca95723dd9c7d2fa68
KarthikeyaGit <sandinenikarthikeya@gmail.com>
2021-11-08 09:59:55 +0530

    login res fix

--- Controllers/Login.Controller.js
+++ Controllers/Login.Controller.js
@@ -3,9 +3,23 @@ const Login = require("../Models/Login.Model");
 
 
 exports.login = (req, res) => {
-    Login.find({email: req.body.email, password: req.body.password})
+    console.log("req", req.body)
+    Login.findOne({email: req.body.email, password: req.body.password})
     .then((data) => {
-      res.send(data);
+      if(data === null) {
+        console.log("data", data)
+        res.send({
+          hasError: true,
+          message:"Invalid Email or Password"
+        });
+      } else {
+        res.status(200).send(
+          {
+            hasError: false,
+            message:"Logged in successfully"
+          })
+      }
+    
     })
     .catch((err) => {
       res.status(500).send({
